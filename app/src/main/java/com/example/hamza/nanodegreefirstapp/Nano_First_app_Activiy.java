78ddeef48cb59d226fbd47976637674696e6b015
Muhammad Hamza <muhammad00hamza@gmail.com>
2016-10-11 03:59:31 -0700

    NanoDegree First App

---
+++ app/src/main/java/com/example/hamza/nanodegreefirstapp/Nano_First_app_Activiy.java
@@ -0,0 +1,13 @@
+package com.example.hamza.nanodegreefirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Nano_First_app_Activiy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nano__first_app__activiy);
+    }
+}
